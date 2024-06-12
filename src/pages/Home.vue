f9323524e9ba9ecdd03065f262b2a6d1d317d493
Emurillo <elkin.murillo@virtualsoft.tech>
2024-06-12 14:47:37 -0500

    changes in home structure app list and styles colors

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,7 +22,6 @@ const onAddTask = async (): Promise<void> => {
     const response = await onAddSaveTask(params.data);
 
     console.log(response);
-    
   }
 };
 
@@ -41,48 +40,56 @@ const getDataTask = async (): Promise<void> => {
   <div class="home-background bg-[#E3F2FD]">
     <div class="wrapper">
       <header>Todo App</header>
-      <div class="inputField flex gap-1">
-        <input
-          type="text"
-          placeholder="Add your new todo"
-          v-model="descriptionTask"
-          @keyup.enter="onAddTask()"
-        />
-        <button
-          class="w-[50px] h-[100%] bg-[#721ce3] hover:bg-[#8e49e8] hover:scale-[1.1] rounded-md text-white"
-          @click.stop="onAddTask()"
-        >
-          <i class="fas fa-plus"></i>
-        </button>
-      </div>
-      <div>
-        <input
-          type="button"
-          class="text-white bg-green-400 hover:bg-green-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-          value="Iniciadas"
-        />
-        <input
-          type="button"
-          class="text-white bg-blue-400 hover:bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-          value="Terminadas"
-        />
-        <input
-          type="button"
-          class="text-white bg-orange-300 hover:bg-orange-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-          value="Eliminadas"
-        />
-      </div>
+      <div class="flex flex-col gap-[1em] w-full h-full">
+        <!-----INPUT ADD-->
+        <div class="inputField flex gap-1">
+          <input
+            type="text"
+            placeholder="Add your new todo"
+            v-model="descriptionTask"
+            @keyup.enter="onAddTask()"
+          />
+          <button
+            class="w-[50px] h-[100%] bg-[#721ce3] hover:bg-[#8e49e8] hover:scale-[1.1] rounded-md text-white"
+            @click.stop="onAddTask()"
+          >
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
+        <!----BUTTONS--->
+        <div class="flex justify-center items-center">
+          <input
+            type="button"
+            class="text-white bg-green-400 hover:bg-green-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            value="Iniciadas"
+          />
+          <input
+            type="button"
+            class="text-white bg-blue-400 hover:bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            value="Terminadas"
+          />
+          <input
+            type="button"
+            class="text-white bg-orange-300 hover:bg-orange-500 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            value="Eliminadas"
+          />
+        </div>
+        <!----BUTTONS-->
 
-      <div class="containerList h-[256px] overflow-y-scroll">
-        <ul class="todoList flex flex-col gap-[0.65em]">
-          <li v-for="n in 20">
-            <task-list />
-          </li>
-        </ul>
-      </div>
-      <div class="footer">
-        <span>You have <span class="pendingTasks"></span> pending tasks</span>
-        <button @click.stop="getDataTask">Clear All</button>
+        <!----TASK-->
+        <div class="containerList h-[270px] overflow-y-scroll">
+          <ul class="todoList flex flex-col gap-[0.65em]">
+            <li v-for="n in 20">
+              <task-list />
+            </li>
+          </ul>
+        </div>
+        <!----TASK-->
+
+        <div class="footer">
+          <span>You have <span class="pendingTasks"></span> pending tasks</span>
+          <button @click.stop="getDataTask">Clear All</button>
+        </div>
       </div>
     </div>
   </div>
@@ -101,11 +108,14 @@ const getDataTask = async (): Promise<void> => {
   background: #fff;
   max-width: 400px;
   width: 100%;
-  height: 30em;
+  height: 36em;
   padding: 25px;
   border-radius: 5px;
   box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
   margin: 20px;
+  display: flex;
+  flex-direction: column; /* Para que los elementos dentro del wrapper se apilen verticalmente */
+  justify-content: center; /* Para centrar verticalmente el contenido */
 }
 
 .wrapper header {
@@ -135,35 +145,11 @@ const getDataTask = async (): Promise<void> => {
   border-color: #8e49e8;
 }
 
-/*
-.inputField button {
-  width: 50px;
-  height: 100%;
-  border: none;
-  color: #fff;
-  margin-left: 5px;
-  font-size: 21px;
-  outline: none;
-  background: #8e49e8;
-  cursor: pointer;
-  border-radius: 3px;
-  opacity: 0.6;
-  pointer-events: none;
-  transition: all 0.3s ease;
-}
-
-.inputField button:hover,
-.footer button:hover {
-  background: #721ce3;
-}
-/* 
-.inputField button.active {
-  opacity: 1;
-  pointer-events: auto;
-} */
-
-.footer {
-  margin-top: 20px; /* Aseguramos que el pie de página esté separado del contenido */
+.footer{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2em;
 }
 
 .footer button {
@@ -192,14 +178,11 @@ const getDataTask = async (): Promise<void> => {
 }
 
 .containerList {
-  /* Styles for the container holding the scrollable content */
   position: relative;
 }
 
 .containerList::-webkit-scrollbar {
-  width: 8px; /* Tamaño del scroll en vertical */
-  height: 8px; /* Tamaño del scroll en horizontal */
-  /* display: none;  Ocultar scroll */
+  height: 8px;
 }
 
 .containerList::-webkit-scrollbar-thumb {
@@ -207,13 +190,11 @@ const getDataTask = async (): Promise<void> => {
   border-radius: 4px;
 }
 
-/* Cambiamos el fondo y agregamos una sombra cuando esté en hover */
 .containerList::-webkit-scrollbar-thumb:hover {
   background: #b3b3b3;
   box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.2);
 }
 
-/* Cambiamos el fondo cuando esté en active */
 .containerList::-webkit-scrollbar-thumb:active {
   background-color: #999999;
 }
@@ -223,13 +204,11 @@ const getDataTask = async (): Promise<void> => {
   border-radius: 4px;
 }
 
-/* Cambiamos el fondo cuando esté en active o hover */
 .containerList::-webkit-scrollbar-track:hover,
 .containerList::-webkit-scrollbar-track:active {
   background: #d4d4d4;
 }
 
-/* Styles for the ul inside the scrollable container */
 .todoList {
   margin: 0;
   padding: 0;
