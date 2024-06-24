5bd3bf5ae5b0d45df9ea88f869965369723ce5cf
Elkin Murillo <rastaw27@gmail.com>
2024-06-24 00:26:03 -0500

    new design in login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -32,167 +32,249 @@ const login = async () => {
 const showbutton = (): void => {};
 </script>
 <template>
-  <div class="relative h-[100vh] grid items-center">
-    <img
-      src="../assets/img/login-bg.png"
-      alt="image"
-      class="absolute w-[100%] h-[100%] object-fit object-center"
-    />
+  <div class="container">
+    <div class="forms-container">
+      <div class="signin-signup">
+        <form action="" class="sig-in-form">
+          <h2 class="title">Sing in</h2>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" />
+          </div>
+          <input type="submit" value="Login" class="btn solid" />
+          <p class="social-text">Or Sing in with social plataforms</p>
+          <div class="social-media">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+        </form>
 
-    <form
-      @submit.prevent="login"
-      action=""
-      class="relative w-[420px] bg-opacity-10 border border-white border-opacity-70 p-10 text-white rounded-xl backdrop-blur-2xl m-auto"
-    >
-      <h1 class="text-center text-[2rem] mb-[1.25rem]">Login</h1>
-
-      <div class="login__inputs">
-        <div class="login__box">
-          <input
-            type="text"
-            v-model="username"
-            placeholder="usuario@ejemplo.com"
-            class="login__input"
-          />
-
-          <i class="ri-mail-fill"></i>
-        </div>
-        <div class="login__box">
-          <input
-            type="password"
-            v-model="password"
-            placeholder="********"
-            class="login__input"
-          />
-
-          <i class="ri-lock-2-fill"></i>
-        </div>
+        <form action="#" class="sign-up-form">
+          <h2 class="title">Sign up</h2>
+          <div class="input-field">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-envelope"></i>
+            <input type="email" placeholder="Email" />
+          </div>
+          <div class="input-field">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" />
+          </div>
+          <input type="submit" class="btn" value="Sign up" />
+          <p class="social-text">Or Sign up with social platforms</p>
+          <div class="social-media">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+        </form>
       </div>
+    </div>
 
-      <div class="login__check">
-        <div class="login__check-box">
-          <input type="checkbox" class="login__check-input" id="user-check" />
-          <label for="user-check" class="login__check-label">Remember me</label>
+    <div class="panels-container">
+      <div class="panel left-panel">
+        <div class="content">
+          <h3>New here ?</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+            ex ratione. Aliquid!
+          </p>
+          <button class="btn transparent" id="sign-up-btn">Sign up</button>
         </div>
-        <a href="#" class="login__forgot">Forgot Password?</a>
+        <img src="../assets/img/log.svg" class="image" alt="" />
       </div>
-
-      <button type="submit" class="login__button">Login</button>
-
-      <div class="login__register">
-        Don´t have an account? <a href="#">Register</a>
+      <div class="panel right-panel">
+        <div class="content">
+          <h3>One of us ?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            laboriosam ad deleniti.
+          </p>
+          <button class="btn transparent" id="sign-in-btn">Sign in</button>
+        </div>
+        <img src="../assets/img/register.svg" class="image" alt="" />
       </div>
-    </form>
+    </div>
   </div>
-
-  <show-alert />
-
-  <button @click="showbutton">ALERTS</button>
 </template>
 <style scoped>
-.login__inputs,
-.login__box {
+.container {
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fff;
+  overflow: hidden;
+}
+
+.container::before {
+  content: "";
+  position: absolute;
+  width: 2000px;
+  height: 2000px;
+  border-radius: 50%;
+  background: linear-gradient(-45deg, #4481eb, #04befe);
+  top: -10%;
+  right: 48%;
+  transform: translateY(-50%);
+}
+
+.forms-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.signin-sign {
+  position: absolute;
+  top: 50%;
+  left: 75%;
+  transform: translate(-50%, -50%);
+  width: 50%;
   display: grid;
+  grid-template-columns: 1fr;
 }
 
-.login__inputs {
-  row-gap: 1.25rem;
-  margin-bottom: 1rem;
-}
-
-.login__box {
-  grid-template-columns: 1fr max-content;
-  column-gap: 0.75rem;
+form {
+  display: flex;
   align-items: center;
-  border: 2px solid hsla(0, 0%, 100%, 0.7);
-  padding-inline: 1.25rem;
-  border-radius: 4rem;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 5rem;
+  overflow: hidden;
+  grid-column: 1 / 2;
+  grid-row: 1 / 2;
 }
 
-.login__input,
-.login__button {
+form.signin-sign {
+  z-index: 2;
+}
+
+form.sign-up-form {
+  z-index: 1;
+  opacity: 0;
+}
+
+.title {
+  font-size: 2.2rem;
+  color: #444;
+  margin-bottom: 10px;
+}
+
+.input-field {
+  max-width: 380px;
+  width: 100%;
+  height: 55px;
+  background-color: #f0f0f0;
+  margin: 10px 0;
+  border-radius: 55px;
+  display: grid;
+  grid-template-columns: 15% 85%;
+  padding: 0 0.4rem;
+}
+.input-field i {
+  text-align: center;
+  line-height: 55px;
+  color: #acacac;
+  font-size: 1.1rem;
+}
+
+.input-field input {
+  background: none;
+  outline: none;
+  border: none;
+}
+
+.input-field input {
+  background: none;
+  outline: none;
+  border: none;
+  line-height: 1;
+  font-weight: 600;
+  font-size: 1.1rem;
+  color: #333;
+}
+
+.input-field input::placeholder {
+  color: #aaa;
+  font-weight: 500;
+}
+
+.btn {
+  width: 150px;
+  height: 49px;
   border: none;
   outline: none;
-}
-
-.login__input {
-  width: 100%;
-  background: none;
-  color: var(--white-color);
-  padding-block: 1rem;
-}
-
-.login__input::placeholder {
-  color: var(--white-color);
-}
-
-.login__box i {
-  font-size: 1.25rem;
-}
-
-.login__check,
-.login__check-box {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.login__check-box {
-  column-gap: 0.5rem;
-}
-
-.login__check-input {
-  width: 1rem;
-  height: 1rem;
-  accent-color: var(--white-color);
-}
-
-.login__forgot {
-  color: var(--white-color);
-}
-
-.login__forgot:hover {
-  text-decoration: underline;
-}
-
-.login__button {
-  width: 100%;
-  padding: 1rem;
-  margin-bottom: 1rem;
-  background-color: var(--white-color);
-  border-radius: 4rem;
-  color: var(--black-color);
-  font-weight: 500;
+  border-radius: 49px;
   cursor: pointer;
-  position: relative;
-  top: 0.65rem;
+  background-color: #5995fd;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  margin: 10px 0;
+  transition: 0.5s;
 }
 
-.login__register {
-  font-size: var(--small-font-size);
-  text-align: center;
+.btn:hover {
+  background-color: #4d84e2;
 }
 
-.login__register a {
-  color: var(--white-color);
-  font-weight: 500;
+.social-text {
+  padding: 0.7rem 0;
+  font-size: 1rem;
 }
 
-.login__register a:hover {
-  text-decoration: underline;
+.social-media {
+  display: flex;
+  justify-content: center;
 }
 
-/**===================BREACKPOINTS======================= */
-@media screen and (min-width: 576px) {
-  .login {
-    justify-content: center;
-  }
-  .login__form {
-    width: 420px;
-    padding-inline: 2.5rem;
-  }
-  .login__title {
-    margin-bottom: 2rem;
-  }
+.social-icon {
+  height: 46px;
+  width: 46px;
+  border: 1px solid #333;
+  margin: 0 0.45rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: #333;
+  font-size: 1.1rem;
+  border-radius: 50%;
+  transition: 0.3s;
 }
+
+.social-icon:hover {
+  color: #4481eb;
+  border-color: #4481eb;
+}
+
+/**REGISTER */
 </style>
